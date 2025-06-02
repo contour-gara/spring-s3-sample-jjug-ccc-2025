04f7a1da5c18b5fa36d94bdf56535d1c90958593
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-06-03 08:26:43 +0900

    AWS のコンフィグクラス作成

---
+++ sample/src/main/java/org/contourgara/AwsConfig.java
@@ -0,0 +1,23 @@
+package org.contourgara;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "aws")
+@Getter
+@Setter
+public class AwsConfig {
+    private String accessKeyId;
+    private String secretKey;
+    private String region;
+    private final S3Config s3 = new S3Config();
+
+    @Getter
+    @Setter
+    public static class S3Config {
+        private String endpoint;
+    }
+}
--- sample/src/test/java/org/contourgara/SampleApplicationTest.java
+++ sample/src/test/java/org/contourgara/SampleApplicationTest.java
@@ -1,10 +1,18 @@
 package org.contourgara;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SampleApplicationTest {
+    @Autowired
+    AwsConfig awsConfig;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertThat(awsConfig).isNotNull();
+    }
 }
